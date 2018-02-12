6b2599a4b703004e02d2c7561816240ec0c1a758
Alfred Lopez <alfred.e.lopez@gmail.com>
2018-02-12 20:46:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function logWhisper(string) {console.log(string).toLowerCase())}
+function logWhisper(string) {console.log(string.toLowerCase())}
 function logShout(string) {console.log(string.toUpperCase())}
 function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
