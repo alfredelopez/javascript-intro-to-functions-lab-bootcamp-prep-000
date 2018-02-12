3768836030fe2ddf6ded5931ec41cf747405b8ec
Alfred Lopez <alfred.e.lopez@gmail.com>
2018-02-12 20:37:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+
+shout('hello')
+function logShout(string) {console.log(string.toUppercase())}
+function shout(string) {return string.toUpperCase()}
+function whisper(string) {return string.toLowerCase()}
+function sayHiToGrandma(string) {if (string === string.toUpperCase()) return "YES INDEED!"; else if (string === string.toLowerCase()) return "I can't hear you!"; else (string === "I love you, Grandma."); return "I love you, too."}
